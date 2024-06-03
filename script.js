5e77ed08f33073769b15ea1dff81c70297e8ca0a
Priyashan Gimantha <120010882+EroseEmrys@users.noreply.github.com>
2024-06-04 00:13:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show() {
+    mainMenu.style.display = 'flex';
+}
+
+function close() {
+    mainMenu.style.display = 'none';
+}
